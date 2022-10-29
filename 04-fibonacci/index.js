4552cc3b133e0944d65fb5d92f6e4c15977bad49
MarcelloMartins <marcello08martins@outlook.com>
2022-10-29 16:28:45 -0300

    feat(04-fibonacci):completead fourth challenge

--- 04-fibonacci/index.js
+++ 04-fibonacci/index.js
@@ -1,4 +1,10 @@
 export function fibonacci(value) {
   // implementar logica aqui
-  
+  if(value < 0) return 0;
+  if(value < 2) return value;
+  if(memo[value]) return memo[value];
+  let result = fibonacci(value - 1, memo) + fibonacci(value - 2, memo);
+  memo[value] = result;
+  console.log(memo);
+  return result;
 }
