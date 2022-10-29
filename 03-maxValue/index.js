82346e67caea377a800f7737d6a5410fe1981318
MarcelloMartins <marcello08martins@outlook.com>
2022-10-29 16:25:05 -0300

    feat(03-maxvalue):completead third challenge

--- 03-maxValue/index.js
+++ 03-maxValue/index.js
@@ -1,4 +1,14 @@
 export function maxValue(values) {
   // implementar logica aqui
-  
+  if (values.length == 0){
+    return 0;
+  }
+  return values.reduce((maiornum,atualnum) => {
+    if(maiornum > atualnum){
+      return maiornum;
+    }
+    else{
+      return atualnum;
+    }
+  })
 }
