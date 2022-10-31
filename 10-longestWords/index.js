146c2a9cc322e860e0e9ee78ea4d72a403c63b91
MarcelloMartins <marcello08martins@outlook.com>
2022-10-31 18:28:35 -0300

    feat(10-longestWords):completead tenth challenge

--- 10-longestWords/index.js
+++ 10-longestWords/index.js
@@ -1,4 +1,12 @@
 export function longestWords(words) {
     // implementar logica aqui
-    
+    let lista = [];
+    words.forEach(palavra => {
+        if(!lista[0] || lista[0].length < palavra.length){
+            lista = [palavra]
+        } else if(lista[0].length == palavra.length){
+            lista.push(palavra)
+        }
+    });
+    return lista;
 }
